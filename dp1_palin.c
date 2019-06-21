9d98e13b8eb719d546952a9cfc2cc8b343828393
Abhinav Sharma <43822140+AbhinavRsharma@users.noreply.github.com>
2019-06-21 13:06:33 +0530

    Update dp1_palin.c

--- dp1_palin.c
+++ dp1_palin.c
@@ -2,13 +2,7 @@
 #include<string.h>
 
 void longest_(int *longest, char* str, int i, int j, int l){
-    if(i == 0){
-        if(str[i] == str[j+1])
-            *(longest+(l-1)*i+j) = 1;
-        else
-            *(longest+(l-1)*i+j) = 0;
-    }
-    else if(j == l-2){
+    if(i == 0 || j == l-2){
         if(str[i] == str[j+1])
             *(longest+(l-1)*i+j) = 1;
         else
@@ -20,11 +14,10 @@ void longest_(int *longest, char* str, int i, int j, int l){
         *(longest+(l-1)*i+j) = 0;
 }
 
-
 int main(){
     char str[101];
     scanf("%s",str);
-    int l = strlen(str);
+    int l = strlen(str), count = 0;
     int longest[l-1][l-1], pre[l-1][l-1], suf[l-1][l-1], palin[l-1][l-1] ;
     for(int i=0; i<l-1; i++){
         for(int j=0;j<l-1;j++){
@@ -34,33 +27,27 @@ int main(){
             palin[i][j]=-1;
         }
     }
+    
     for(int i=0; i<l-1; i++){
         for(int j=i+1;j<l-1;j++){
             longest_(longest, str, i, j, l);
         }
-    }
-    for(int i=0; i<l-1; i++){
-        for(int j=i+1;j<l-1;j++){
-            printf("%d ",longest[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    
-    for(int i=0; i<l-2;i++){
-        pre[i][i] = 0;
-        suf[i][i] = 0;
+    }    
+    for(int i=0; i<l-1;i++){
+        pre[i][i] = 2;
+        suf[i][i] = 2;
         palin[i][i] = 1;
-        if(i+1<l-2)
-            palin[i][i+1] = 1;
+        if(i+1<l-1)
+            palin[i+1][i] = 1;
     }
     
-    for(int k=1; k<l-2; k++){
-        for(int i=0; i+k<l-2; i++){
+    for(int k=1; k<=l-2; k++){
+        for(int i=0; i+k<l-1; i++){
             if(str[i+1] != str[i+k])
                 palin[i][i+k] = 0;
             else
-                palin[i][i+k] = palin[i][i+k];
+                palin[i][i+k] = palin[i+1][i+k-1];
+            
             pre[i][i+k] = palin[i][i+k] + pre[i][i+k-1];
             suf[i][i+k] = palin[i][i+k] + suf[i+1][i+k];
             
@@ -69,25 +56,9 @@ int main(){
     
     for(int i=0; i<l-1; i++){
         for(int j=i+1;j<l-1;j++){
-            printf("%d ",palin[i][j]);
+            count += longest[i][j]*(pre[i][j]+suf[i][j]-1);
         }
-        printf("\n");
     }
-    printf("\n");
-    for(int i=0; i<l-1; i++){
-        for(int j=i+1;j<l-1;j++){
-            printf("%d ",suf[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    for(int i=0; i<l-1; i++){
-        for(int j=i+1;j<l-1;j++){
-            printf("%d ",pre[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-    
+    printf("%d", count);
     return 0;
 }
